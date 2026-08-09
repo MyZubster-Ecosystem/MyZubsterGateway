b02deee00a18eb8ed8e6c10c064cd9a6a2ebd88c
laurentketterle-hub <laurentketterle@gmail.com>
2026-08-09 02:53:22 +0200

    feat: add API Docs route (#147) (#930)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import UrbanGardenDashboard from './pages/UrbanGardenDashboard';
 import HospitalDashboard from './pages/HospitalDashboard';
 import TransactionHistory from './pages/TransactionHistory';
+import ApiDocs from './pages/ApiDocs';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/garden" element={<UrbanGardenDashboard />} />
         <Route path="/hospital" element={<HospitalDashboard />} />
         <Route path="/transactions" element={<TransactionHistory />} />
+        <Route path="/api-docs" element={<ApiDocs />} />
       </Routes>
     </BrowserRouter>
   );
